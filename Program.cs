5490bf8a9abb27e49ccd194a1c19eacd810d7230
César Paulangleo <20160120+BooterMan98@users.noreply.github.com>
2026-01-12 16:25:22 -0300

    Added Main program code for execution (WIP)

--- Program.cs
+++ Program.cs
@@ -1,2 +1,30 @@
 ﻿// See https://aka.ms/new-console-template for more information
+using WeatherApp.Interfaces;
+using WeatherApp.Sources;
+using WeatherApp.Weather;
+
 Console.WriteLine("Hello, World!");
+
+// {"Location": "City Name","Temperature": 23.0,"Humidity": 85.0}
+
+/* 
+<WeatherData><Location>City Name</Location><Temperature>23.0</Temperature><Humidity>85.0</Humidity></WeatherData>
+*/
+
+WeatherManagerProvider provider = new ();
+var sources = provider.GetWeatherSources();
+
+var manager = provider.CreateWeatherManager(sources);
+
+foreach (var bot in await provider.CreateBotsAsync("config.json"))
+{
+  manager.Subscribe(bot);
+}
+
+while (true)
+{
+  var newMeasurementString = Console.ReadLine();
+  // var newMeasurement = new JSONWeatherSource().Read(newMeasurementString);
+  manager.ReceiveRawMeasurement(newMeasurementString);
+  manager.NotifySubscribers();
+} 

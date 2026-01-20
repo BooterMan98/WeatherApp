2250120fbe84dd2dc6ddc8d481892c3d2e4ad361
César Paulangleo <20160120+BooterMan98@users.noreply.github.com>
2026-01-20 10:15:07 -0300

    Added first test for Bots
    - RainBot should print a message if measurement passes Humidity threshold

--- WeatherAppTests/BotTests.cs
+++ WeatherAppTests/BotTests.cs
@@ -1,10 +1,59 @@
+using AutoFixture;
+using Moq;
+using WeatherApp.Weather.Bots;
+using WeatherApp.Weather.Models;
+using WeatherApp.Weather.Types;
 namespace WeatherAppTests;
 
 public class BotTests
 {
-    [Fact]
-    public void ShouldCallAnalyzeOnceNotified()
+    /// <summary>
+    /// Verifies that the RainBot prints the configured message to the console when the humidity threshold is achieved or exceeded.
+    /// </summary>
+    /// <param name="threshold">The humidity threshold value to test, ranging from 0 to 100.</param>
+    [Theory]
+    [InlineData(0)]
+    [InlineData(20)]
+    [InlineData(40)]
+    [InlineData(60)]
+    [InlineData(80)]
+    [InlineData(100)]
+    public void RainBotShouldPrintAMessageToConsoleWhenHumidityThresholdAchieved(decimal threshold)
     {
-        Assert.True(false);
+        // arrange
+        var fixture = new Fixture();
+        var humidityThreshold = (Humidity)threshold;
+
+        fixture.Customize<BotConfiguration>(c => c
+            .Without(c => c.TemperatureThreshold)
+            .With(c => c.HumidityThreshold, humidityThreshold)
+        );
+
+        fixture.Customize<WeatherMeasurement>(c => c
+        .With(w => w.Humidity,
+            (Humidity humidity) => (humidity < humidityThreshold) ? humidityThreshold : humidity)
+        );
+
+        var measurement = fixture.Create<WeatherMeasurement>();
+        var botConfiguration = fixture.Create<BotConfiguration>();
+
+        var message = botConfiguration.Message;
+
+        using (StringWriter sw = new ())
+        {
+
+            var output = Console.Out;
+            Console.SetOut(sw);
+
+            // act
+            var bot = new RainBot(botConfiguration);
+            bot.Update(measurement);
+
+            Console.SetOut(output);
+
+            Assert.Equal(message, sw.ToString().Trim());
+            
+        }
+        // dispose
     }
 }
